06dd922edff0ba612856ffaa49801f0e73425ae8
Samuel Ting <sting@u91z05s02.42kl.edu.my>
2024-03-14 14:29:59 +0800

    update

--- 1parent2child.c
+++ 1parent2child.c
@@ -20,16 +20,16 @@ int main()
 	else if (pid == 0)
 	{
 		close(pfd[R_END]); //close unused end (the reading end) of the pipe
-		dup2(pfd[W_END], STDOUT_FILENO); // replace pdf[1] with stdout to become write end of the pipe
+		dup2(pfd[W_END], STDOUT_FILENO); // replace pfd[1] with stdout to become write end of the pipe
 		close(pfd[W_END]);
  		char *args[2] = {"ls", NULL};
-		execve("/bin/ls", args, NULL);
 		if (execve("/bin/ls", args, NULL) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
     	}
 	}
+	// close(pfd[W_END]); // ? close write end of pipe to sign end of input to 'wc' command in 2nd child process
 
 	// --------------
 	int pid2 = fork();
@@ -41,7 +41,7 @@ int main()
 	if (pid2 == 0) // child process 2
 	{
 		close(pfd[W_END]); //close unused end (the writing end) of the pipe
-		dup2(pfd[R_END], STDIN_FILENO); // replace pdf[0] with stdin to become read end of the pipe
+		dup2(pfd[R_END], STDIN_FILENO); // replace pfd[0] with stdin to become read end of the pipe
 		close(pfd[R_END]); //close it immediately as it will no longer be used
  		char *args[2] = {"wc", NULL};
 		if (execve("/usr/bin/wc", args, NULL) == -1)
@@ -51,7 +51,6 @@ int main()
     	}
 	}
 
-	close(pfd[1]); // ? close write end of pipe to sign end of input to 'wc' command in 2nd child process
 	waitpid(pid, NULL, 0);
 	waitpid(pid2, NULL, 0);
 	printf("this msg should appear after child process finish\n");
